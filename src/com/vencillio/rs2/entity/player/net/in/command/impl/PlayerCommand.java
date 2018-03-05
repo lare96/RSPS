32c4a5caefacba76b33417f550282ce5524c3471
tannermon11 <tannermon11@gmail.com>
2018-03-05 05:25:04 -0700

    npcinfo debug

--- src/com/vencillio/rs2/entity/player/net/in/command/impl/PlayerCommand.java
+++ src/com/vencillio/rs2/entity/player/net/in/command/impl/PlayerCommand.java
@@ -96,7 +96,7 @@ public class PlayerCommand implements Command {
 					NpcCombatDefinition def = GameDefinitionLoader.getNpcCombatDefinition(parser.nextInt());
 					NpcCombatDefinition.Skill[] skills = def.getSkills();
 					int hp = 0;
-					for (int i = skills.length; i > 0; i--) {
+					for (int i = skills.length-1; i > 0; i--) {
 						System.out.println("id: " + skills[i].getId() + " lvl: " + skills[i].getLevel());
 						if (skills[i].getId() == 3)
 							hp = skills[i].getLevel();
