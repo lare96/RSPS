11e16614a7de0ebbeb35c63c413fa74d856d6e55
tannermon11 <tannermon11@gmail.com>
2018-03-05 22:22:43 -0700

    drop simulation improvement

--- src/com/vencillio/rs2/entity/player/net/in/command/impl/DeveloperCommand.java
+++ src/com/vencillio/rs2/entity/player/net/in/command/impl/DeveloperCommand.java
@@ -635,7 +635,7 @@ public class DeveloperCommand implements Command {
 				player.send(new SendMessage("Simulated " + trials + " kills of \'" + npcDef.getName() + "\' (Id: " + npc + ")."));
 				if(chance != 0) {
 					Item myItem = new Item(id);
-					player.send(new SendMessage("Chance of " + myItem.getName() + " dropping is " + String.format("%.2d", chance / trials) + "%"));
+					player.send(new SendMessage("Chance of " + myItem.getDefinition().getName() + " dropping is " + String.format("%.2d", chance / trials) + "%"));
 				}
 			}
 			return true;
