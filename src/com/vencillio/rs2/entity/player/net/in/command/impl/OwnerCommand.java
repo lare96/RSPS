3f21b7e1f9280e994cd0b47702144c65449e2913
tannermon11 <tannermon11@gmail.com>
2018-05-29 04:06:07 -0700

    dp implementation

--- src/com/vencillio/rs2/entity/player/net/in/command/impl/OwnerCommand.java
+++ src/com/vencillio/rs2/entity/player/net/in/command/impl/OwnerCommand.java
@@ -155,7 +155,7 @@ public class OwnerCommand implements Command {
 						int rand = Utility.random(7), rand2 = Utility.random(7);
 						GroundItemHandler.add(new GroundItem(new Item(itemToSpawn),
 								new Location(player.getX() + (rand * sign[Utility.random(1)]),
-										player.getY() + (rand2 * sign[Utility.random(1)]))));
+										player.getY() + (rand2 * sign[Utility.random(1)])), 0, null));
 						amount--;
 					}
 
