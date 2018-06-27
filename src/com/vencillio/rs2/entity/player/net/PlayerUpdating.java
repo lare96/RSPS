22a83223e7b2f2f1c709bee07877b50b469f61f3
tannermon11 <tannermon11@gmail.com>
2018-06-27 04:53:41 -0700

    test out set visible

--- src/com/vencillio/rs2/entity/player/net/PlayerUpdating.java
+++ src/com/vencillio/rs2/entity/player/net/PlayerUpdating.java
@@ -234,12 +234,16 @@ public final class PlayerUpdating {
 
 			PlayerUpdateFlags flags = pFlags[i];
 
-			if (flags != null && !flags.getUsername().equals(local.getUsername())){// && flags.isActive()) {
+			if (flags != null && !flags.getUsername().equals(local.getUsername())){ //&& flags.isActive()) {
 
+				System.out.println("flag username: " + flags.getUsername() + "flags.getLocation().isViewableFrom(player.getLocation()" + flags.getLocation().isViewableFrom(player.getLocation()));
+				System.out.println("!doesLocalListContainPlayer(player, flags.getUsernameToLong()): " + !doesLocalListContainPlayer(player, flags.getUsernameToLong()));
 				if (!doesLocalListContainPlayer(player, flags.getUsernameToLong()) && flags.getLocation().isViewableFrom(player.getLocation())) {
 					player.getPlayers().add(World.getPlayers()[i]);
 					addPlayer(out, local, flags, i);
-					updateState(flags, block, true, player.getPrivateMessaging().ignored(flags.getUsername()));
+					if(player.isActive()) {
+						updateState(flags, block, true, player.getPrivateMessaging().ignored(flags.getUsername()));
+					}
 					added++;
 				}
 			}
