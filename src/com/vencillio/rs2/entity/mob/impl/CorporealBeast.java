5a84ae073c1ece7ab0b4f29dec2bcd196ffc8ef3
tannermon11 <tannermon11@gmail.com>
2018-07-18 23:00:09 -0700

    debug corp

--- src/com/vencillio/rs2/entity/mob/impl/CorporealBeast.java
+++ src/com/vencillio/rs2/entity/mob/impl/CorporealBeast.java
@@ -3,21 +3,24 @@ package com.vencillio.rs2.entity.mob.impl;
 import com.vencillio.rs2.content.combat.Hit;
 import com.vencillio.rs2.entity.Location;
 import com.vencillio.rs2.entity.mob.Mob;
+import com.vencillio.rs2.entity.player.Player;
 import com.vencillio.rs2.entity.player.net.out.impl.SendMessage;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CorporealBeast extends Mob {
-	
+
 	private Mob[] darkEnergyCores = null;
 	private long TIME;
 
+	public List<Player> allCombatants = new ArrayList<>();
+
+
 	public CorporealBeast() {
-		super(319, true, new Location(2946, 4386));
+		super(319, true, new Location(2993, 4382, 2));
 		TIME = System.currentTimeMillis();
-		System.out.println("Combatant size(In constructor): " + getCombatants().size());
-		for(int i=0; i<getCombatants().size(); i++)
-			getCombatants().get(i).getAttributes().set("CORP_DAMAGE", 0);
 	}
 
 	public boolean areCoresDead() {
@@ -41,15 +44,34 @@ public class CorporealBeast extends Mob {
 	}
 
 	@Override
+	public void doAliveMobProcessing() {
+		if (getCombatants().size() > 0) {
+			for (int i = 0; i < getCombatants().size(); i++) {
+				if(!allCombatants.contains(getCombatants().get(i))) {
+					allCombatants.add(getCombatants().get(i));
+				}
+				if (!getCombatants().get(i).getAttributes().isSet("CORP_DAMAGE"))
+					getCombatants().get(i).getAttributes().set("CORP_DAMAGE", 0);
+			}
+		}
+		if (getCombatants().size() > 0 && getLevels()[3] <= 0) {
+			System.out.println("Combatant size(In on death): " + getCombatants().size());
+			for (int i = 0; i < getCombatants().size(); i++) {
+				System.out.println("combatant " + i + ": " + getCombatants().get(i).getUsername());
+				getCombatants().get(i).send(new SendMessage("Fight duration: @red@" + new SimpleDateFormat("m:ss").format(System.currentTimeMillis() - TIME) + "</col>."));
+				getCombatants().get(i).send(new SendMessage("Damage dealt: " + getCombatants().get(i).getAttributes().getInt("CORP_DAMAGE")));
+
+			}
+		}
+	}
+
+	@Override
 	public void onDeath() {
 		darkEnergyCores = null;
-		System.out.println("Combatant size(In on death): " + getCombatants().size());
-		for(int i=0; i<getCombatants().size(); i++) {
-			System.out.println("combatant " + i + ": " + getCombatants().get(i).getUsername());
-			getCombatants().get(i).send(new SendMessage("Fight duration: @red@" + new SimpleDateFormat("m:ss").format(System.currentTimeMillis() - TIME) + "</col>."));
-			getCombatants().get(i).send(new SendMessage("Damage dealt: " + getCombatants().get(i).getAttributes().getInt("CORP_DAMAGE")));
+		for(int i=0; i<allCombatants.size(); i++) {
 			getCombatants().get(i).getAttributes().remove("CORP_DAMAGE");
 		}
+		allCombatants.clear();
 	}
 
 	public void spawn() {
