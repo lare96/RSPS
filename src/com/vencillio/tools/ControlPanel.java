67af79570fb90821554f863e7dbdcc59be0c219b
tannermon11 <tannermon11@gmail.com>
2018-08-10 16:01:27 -0700

    debug panel make slayer casket give xp too and fixed it adding slayer points

--- src/com/vencillio/tools/ControlPanel.java
+++ src/com/vencillio/tools/ControlPanel.java
@@ -427,6 +427,7 @@ public class ControlPanel extends JFrame {
 				JDialog inventoryInfo = new JDialog();
 				inventoryInfo.setLayout(new GridBagLayout());
 				inventoryInfo.add(invData);
+
 				invData.setPreferredSize(new Dimension(400,550));
 				//inventoryInfo.setPreferredSize(new Dimension(400, 150));
 				inventoryInfo.pack();
